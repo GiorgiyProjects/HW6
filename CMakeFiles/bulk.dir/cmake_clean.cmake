f18e9b9828ee048d96f228dc15da96084530b06c
GiorgiyProjects <GiorgiyProjects@users.noreply.github.com>
2021-06-29 17:52:29 +0000

    deploy: 0817f09516f23b5be70926903e625d91cb6b5bdf

---
+++ CMakeFiles/bulk.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/bulk.dir/main.cpp.o"
+  "CMakeFiles/bulk.dir/main.cpp.o.d"
+  "bulk"
+  "bulk.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/bulk.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
